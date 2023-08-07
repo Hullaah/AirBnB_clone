58a608d1da35e524d23a070b6629b8c675e888b0
hullaah <umaradelo1.247@gmail.com>
2023-08-07 10:45:00 +0100

    Solved task 4

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,7 @@ class BaseModel:
     Attributes:
         Empty (No attributes)
     """
+
     def __init__(self, *args, **kwargs) -> None:
         """Initialiser of the base class
 
@@ -32,9 +33,18 @@ class BaseModel:
         Return:
             None
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        else:
+            self.id = kwargs["id"]
+            self.created_at = datetime.datetime.fromisoformat(
+                kwargs["created_at"]
+                )
+            self.updated_at = datetime.datetime.fromisoformat(
+                kwargs["updated_at"]
+                )
 
     def save(self) -> None:
         """updates the public instace attribute, updated_at, with the current
@@ -49,7 +59,8 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self) -> dict:
-        """returns a dictionary containing all keys/values of __dict__ of the instance
+        """returns a dictionary containing all keys/values of __dict__ of the
+        instance
 
         Args:
             Empty (No args)
